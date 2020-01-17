28bff52cf2032f6cd71c02a65f7dead44a62ee0a
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2020-01-17 14:33:10 +0100

    Wrap TouchableOpacity to filter extra onPresses on AndroidTV

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-import { Platform, TouchableOpacity } from 'react-native';
+import React, { Component } from "react";
+import { Platform, TouchableOpacity } from "react-native";
+let TouchableFocus = TouchableOpacity;
 
 // on AndroidTV, disable extra native onClick event
-if (Platform.OS === 'android' && Platform.isTV) {
-  TouchableOpacity.defaultProps.clickable = false;
+if (Platform.OS === "android" && Platform.isTV) {
+  class TouchableOpacityAndroidTV extends React.PureComponent {
+    constructor(props) {
+      super(props);
+      this.touchableRef = React.createRef();
+    }
+
+    onPressFilter = e => {
+      const { onPress } = this.props;
+      const { eventKeyAction } = e;
+      if (onPress && eventKeyAction === 0) {
+        onPress(e);
+      }
+    };
+
+    setNativeProps(props) {
+      this.touchableRef.current.setNativeProps(props);
+    }
+
+    render() {
+      return (
+        <TouchableOpacity
+          {...this.props}
+          onPress={this.onPressFilter}
+          ref={this.touchableRef}
+          clickable={false}
+        />
+      );
+    }
+  }
+  TouchableFocus = TouchableOpacityAndroidTV;
 }
 
-export default TouchableOpacity;
+export default TouchableFocus;
